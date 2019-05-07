35652cdd504b1619b643742d0ce95557ee79fb55
vygantas.meskauskis <vygantas.meskauskis@ratesetter.com>
2019-05-07 12:37:38 +0100

    Updates

---
+++ precache-manifest.ea8c2efd2896d23921e85bcb86ba168c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c95e2d9d55c7477eb8fa756da6925ea",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "34dfb9a33f73c72f92b4",
+    "url": "/react-game/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-game/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "34dfb9a33f73c72f92b4",
+    "url": "/react-game/static/js/main.5f445f0b.chunk.js"
+  },
+  {
+    "revision": "3a038c117699a7a341e8",
+    "url": "/react-game/static/js/runtime~main.ef933c85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
